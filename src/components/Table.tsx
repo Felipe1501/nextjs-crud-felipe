cf5d12ae1d86b062788100eda7a74bc2d891c7b0
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-08 11:58:22 -0300

    aula 113 fix: changing table style

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,9 +8,9 @@ export default function Table(props: TableProps){
     function renderationHeader(){
         return (
             <tr>
-                <th>Código</th>
-                <th>Nome</th>
-                <th>Idade</th>
+                <th className="text-left p-4">Código</th>
+                <th className="text-left p-4">Nome</th>
+                <th className="text-left p-4">Idade</th>
             </tr>
 
         )
@@ -19,18 +19,24 @@ export default function Table(props: TableProps){
     function renderationInfo(){
         return props.clients?.map((client, i) =>{
             return (
-                <tr key={client.id}>
-                    <td>{client.id}</td>
-                    <td>{client.name}</td>
-                    <td>{client.idade}</td>
+                <tr 
+                key={client.id}
+                className={`${i % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}`}
+                >
+                    <td className="text-left p-4">{client.id}</td>
+                    <td className="text-left p-4">{client.name}</td>
+                    <td className="text-left p-4">{client.idade}</td>
                 </tr>
             )
         })
     }
 
     return (
-        <table>
-            <thead>
+        <table className="w-full rounded-xl overflow-hidden">
+            <thead className={`
+            text-gray-100
+              bg-gradient-to-r from-purple-500 to-purple-800  
+            `}>
             {renderationHeader()}
             </thead>
             <tbody>
